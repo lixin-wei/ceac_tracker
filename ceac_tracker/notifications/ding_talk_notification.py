59c74c2d12bc05fbfd8ccc4a238249b2a883ef1a
Lixin Wei <wlx65005@gmail.com>
2022-09-16 18:40:28 +0800

    add some data

--- ceac_tracker/notifications/ding_talk_notification.py
+++ ceac_tracker/notifications/ding_talk_notification.py
@@ -1,11 +1,11 @@
 import requests
 from ceac_tracker.config.keys import get_keys
 
-def send_notification(message):
+def send_notification(message, recevier):
     ding_talk_key = get_keys()["ding_talk_key"]
     requests.post(
         f"https://oapi.dingtalk.com/robot/send?access_token={ding_talk_key}",
-        json={"msgtype": "text", "text": {"content": f"Notice: {message}"}},
+        json={"msgtype": "text", "text": {"content": f"Notice: recevier={recevier}, msg={message}"}},
     )
 
 if __name__ == "__main__":
