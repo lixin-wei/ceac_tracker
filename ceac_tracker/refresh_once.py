49a37692ee2f998d61ad4c580256a0439ffed30e
Lixin Wei <wlx65005@gmail.com>
2022-09-17 00:38:32 +0800

    also send to dingtalk

--- ceac_tracker/refresh_once.py
+++ ceac_tracker/refresh_once.py
@@ -2,6 +2,7 @@ from ceac_tracker.request import query_status
 from ceac_tracker.db.sqlite_store import get_all_applications, get_all_records, add_record
 import json
 from ceac_tracker.notifications.email_notification import send_notification
+import ceac_tracker.notifications.ding_talk_notification as ding_talk
 from ceac_tracker.utils.my_logging import get_logger
 
 logger = get_logger(__file__)
@@ -16,10 +17,9 @@ def refresh_once():
 
         if not all_records or all_records[0] != res_tuple:
             logger.info("Inserting new value!")
-            send_notification(
-                f"New visa status: {res['status']}\n\nDescription: {res['description']}\n\nCase last updated: {res['case_last_updated']}",
-                receiver=notification_email
-            )
+            msg = f"New visa status for {res['application_num']}: {res['status']}\n\nDescription: {res['description']}\n\nCase last updated: {res['case_last_updated']}"
+            send_notification(msg, receiver=notification_email)
+            ding_talk.send_notification(msg, receiver)
             add_record(application_id, res["case_last_updated"], res["status"], res["description"])
         else:
             logger.info(f"Application {application_id} has no update now.")
